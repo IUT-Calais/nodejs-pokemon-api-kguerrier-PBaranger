d0c8d87a4d65a9b838d3359a14951c83a9f841f7
BARANGER Phoedora <phoedora.baranger@gmail.com>
2025-03-05 10:58:40 +0100

    modif

--- src/pokemon/pokemon.router.ts
+++ src/pokemon/pokemon.router.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { getPokemons, getPokemonId, postPokemonCreate, deletePokemonId, updatePokemonId } from './pokemon.controller';
 
 export const pokemonRouter = Router();
-// Route pour obtenir la liste des utilisateurs
+// Route pour obtenir la liste des pokemon
 pokemonRouter.get('/', getPokemons);
 pokemonRouter.get('/:pokemonCardId', getPokemonId);
 pokemonRouter.post('/create', postPokemonCreate)
