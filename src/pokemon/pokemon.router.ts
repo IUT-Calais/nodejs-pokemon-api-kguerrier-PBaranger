6884e78f204bec0cb27a5f7a046790ec4b7d5390
BARANGER Phoedora <phoedora.baranger@gmail.com>
2025-03-05 15:58:11 +0100

    modif

--- src/pokemon/pokemon.router.ts
+++ src/pokemon/pokemon.router.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { getPokemons, getPokemonId, postPokemonCreate, deletePokemonId, updatePokemonId } from './pokemon.controller';
+import { verify } from 'crypto';
+import { verifyJWT } from '../common/jwt.middleware';
 
 export const pokemonRouter = Router();
-// Route pour obtenir la liste des pokemon
-pokemonRouter.get('/', getPokemons);
+
+// pokemonRouter.get('/', getPokemons);
 pokemonRouter.get('/:pokemonCardId', getPokemonId);
 pokemonRouter.post('/create', postPokemonCreate)
-pokemonRouter.delete('/delete/:pokemonCardId', deletePokemonId)
-pokemonRouter.patch('/update/:pokemonCardId', updatePokemonId)
+// pokemonRouter.delete('/delete/:pokemonCardId', deletePokemonId)
+// pokemonRouter.patch('/update/:pokemonCardId', updatePokemonId)
+
+pokemonRouter.post('/', verifyJWT, getPokemons)
+pokemonRouter.delete('/delete/:pokemonCardId', verifyJWT, deletePokemonId)
+pokemonRouter.patch('/update/:pokemonCardId', verifyJWT, updatePokemonId)
