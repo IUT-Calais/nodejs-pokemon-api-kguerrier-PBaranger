e76b3da9c2508fc366779d6d3a35f2f6e6287566
BARANGER Phoedora <phoedora.baranger@gmail.com>
2025-05-12 15:43:45 +0200

    modif

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -2,26 +2,44 @@ import request from 'supertest';
 import { app } from '../src';
 import { prismaMock } from './jest.setup';
 
+const bcrypt = require("bcrypt")
+
 describe('User API', () => {
   describe('POST /users', () => {
     it('should create a new user', async () => {
-      const createdUser = {};
+      const hashedPassword = await bcrypt.hash("admin", 10);
+      const createdUser = {
+        "id": 2,
+        "email": "test10creation@mail.com",
+        "password": hashedPassword
+      };
 
+      prismaMock.user.findUnique.mockResolvedValue(createdUser);
+      prismaMock.user.create.mockResolvedValue(createdUser);
+
+      const response = await request(app).post('/users');
       expect(response.status).toBe(201);
       expect(response.body).toEqual(createdUser);
     });
   });
 
-  describe('POST /login', () => {
-    it('should login a user and return a token', async () => {
-      const user = {};
-      const token = 'mockedToken';
+  // describe('POST /users/login', () => {
+  //   it('should login a user and return a token', async () => {
+  //     const hashedPassword = await bcrypt.hash("admin", 10);
+  //     const userTest = {
+  //       "id": 2,
+  //       "email": "test10creation@mail.com",
+  //       "password": hashedPassword
+  //     };
+  //     const token = 'mockedToken';
 
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual({
-        token,
-        message: 'Connexion réussie',
-      });
-    });
-  });
+  //     prismaMock.user.findUnique.mockResolvedValue(userTest);
+  //     const response = await request(app).get('/users/login');
+  //     expect(response.status).toBe(200);
+  //     expect(response.body).toEqual({
+  //       token,
+  //       message: 'Connexion réussie',
+  //     });
+  //   });
+  // });
 });
