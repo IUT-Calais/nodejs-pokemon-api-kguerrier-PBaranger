d866789fd1811f0e41b0718122c0baf3d7cb02b9
BARANGER Phoedora <phoedora.baranger@gmail.com>
2025-05-25 18:43:11 +0200

    ajout swager + modif index

--- src/index.ts
+++ src/index.ts
@@ -13,22 +13,11 @@ const prisma = new PrismaClient();
 
 app.use(express.json());
 
-// import {userRoute}
-// app.use('/user', userRoute)
-
 export const server = app.listen(port);
 
 export function stopServer() {
   server.close();
 }
-
-// app.get('/pokemons-cards', (_req: Request, res: Response) => {
-//   res.status(200).send(`Liste des pokemon`);
-// });
-
-// app.get('/pokemons-cards/:pokemonCardId', (_req: Request, res: Response) => {
-// res.status(200).send(`Pokemon ${_req.params.pokemonCardId}`);
-// });
 
 app.use('/pokemon-cards', pokemonRouter);
 app.use('/users', userRouter);
@@ -37,72 +26,3 @@ app.use('/users', userRouter);
 const swaggerDocument = YAML.load(path.join(__dirname, './swagger.yaml'));
 // Et on affecte le Serveur Swagger UI à l'adresse /api-docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// app.get('/pokemons-cards/', async (_req, res) => {
-//   const poke = await prisma.pokemonCard.findMany({ include: { type: true } });
-//   res.status(200).send(poke);
-// });
-
-
-// app.get('/pokemon-cards/:pokemonCardId', async (_req, res) => {
-//   const { pokemonCardId } = _req.params
-//   const post = await prisma.pokemonCard.findUnique({
-//     where: { id: Number(pokemonCardId) },
-//   })
-//   res.json(post)
-// });
-
-
-// app.post('/pokemon-cards/create', async (_req, res) => {
-//   const { name, pokedexld, type, lifePoint, weight, size, imageUrl } = _req.body
-//   const poke_create = await prisma.pokemonCard.create({
-//     data: {
-//       name,
-//       pokedexld,
-//       type: { connect: { id: type } },
-//       lifePoint,
-//       weight,
-//       size,
-//       imageUrl,
-//     },
-//   });
-//   res.status(200).send(poke_create);
-// });
-
-
-// app.delete(`/pokemon-cards/delete/:id`, async (req, res) => {
-//   const { id } = req.params
-//   const post = await prisma.pokemonCard.delete({
-//     where: { id: Number(id) },
-//   })
-//   res.json(post)
-// })
-
-// app.patch(`/pokemon-cards/update/:id`, async (_req, res) => {
-//   const { id } = _req.params
-//   const updateUser = await prisma.pokemonCard.update({
-//     where: { id: Number(id) },
-//     data: { name: 'oiuyt', },
-//   })
-//   res.json(updateUser)
-// })
-
-// app.patch(`/pokemon-cards/update/:id1`, async (_req, res) => {
-// const { pokemonCardId } = _req.params
-// const { name, pokedexld, type, lifePoint, weight, size, imageUrl } = _req.body
-
-// const post = await prisma.pokemonCard.update({
-//     where: { id: Number(pokemonCardId) },
-//     data: {
-//         name,
-//         pokedexld,
-//         type: { connect: { id: type } },
-//         lifePoint,
-//         weight,
-//         size,
-//         imageUrl,
-//     },
-// })
-// res.json(post)
-
-// })
